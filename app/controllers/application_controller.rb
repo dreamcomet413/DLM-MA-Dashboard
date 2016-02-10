e68639c23e889aedfb79a929e2f12dd53e1f385d
pracdev <tonyfire0108@gmail.com>
2016-02-10 20:39:08 +0100

    csrf token issue was fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   layout :layout_by_resource
 
