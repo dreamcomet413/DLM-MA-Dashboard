8c131e92f0efd5c7527690631f3a7cfec0144496
pracdev <tonyfire0108@gmail.com>
2016-02-10 20:07:09 +0100

    lead create layout created

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,17 +3,27 @@ class LeadsController < ApplicationController
   end
 
   def show
+    @lead = Lead.find(params[:id])
+  end
+
+  def new
+    @lead = Lead.new
   end
 
   def create
     @lead = Lead.new(lead_params)
-
-
+    if @lead.save 
+      flash[:success] = 'Successfully saved.'
+      redirect_to @lead
+    else
+      flash[:error] = 'Error save.'
+      redirect_to @lead
+    end
   end
 
   private
     def lead_params
-      params.permit(:first_name, :last_name, :interest_procedure, :email, :phone, :comment, 
+      params.require(:lead).permit(:first_name, :last_name, :interest_procedure, :email, :phone, :comment, 
                                    :doctor, :consultation_date, :sugery_date)
     end
 end
